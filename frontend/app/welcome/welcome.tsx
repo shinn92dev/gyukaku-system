406879d672e464fa41ece6fa5588c726f2889fad
Anthony Shin <shinn92dev@gmail.com>
2025-10-27 17:28:38 -0700

    💄 style(frontend): initialize ShadCN UI

--- frontend/app/welcome/welcome.tsx
+++ frontend/app/welcome/welcome.tsx
@@ -1,9 +1,11 @@
+import { Button } from "~/components/ui/button";
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
+      <Button>HELLO</Button>
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
