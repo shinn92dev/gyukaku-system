6fb057549e7b20e733d0918088736da015a5defd
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2025-10-30 20:26:32 -0700

    📝 Doc: add bearer token authentication to OpenAPI spec

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -5,7 +5,13 @@ namespace App\Http\Controllers;
 use OpenApi\Attributes as OA;
 
 #[OA\Info(title: 'Gyukaku System RESTful API', version: '1.0.0')]
-#[OA\Server(url: 'http://localhost:8000/api')]
+#[OA\Server(url: 'http://localhost:8000/')]
+#[OA\SecurityScheme(
+    securityScheme: 'bearerAuth',
+    type: 'http',
+    scheme: 'bearer',
+    bearerFormat: 'JWT'
+)]
 abstract class Controller
 {
     //
