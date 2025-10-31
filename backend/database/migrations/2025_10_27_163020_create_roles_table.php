5ef25f705f68107de5a3bfdccb5538abc57e71dd
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2025-10-30 20:32:53 -0700

    🗃️ Db: rename name field to code in roles table

--- backend/database/migrations/2025_10_27_163020_create_roles_table.php
+++ backend/database/migrations/2025_10_27_163020_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50)->unique(); // host, kitchen
+            $table->string('code', 3)->unique(); // hst, ktc
             $table->string('display_name', 50)->unique(); // Host/Hostess, Kitchen Staff
             $table->timestamps();
         });
